52c865b18925a01e6a6bf2b46b2f2c66b8eaedba
Fromis999 <100695795+Fromis999@users.noreply.github.com>
2022-05-04 09:21:54 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+  include_package_data=True,
+  name='pypicalc124',
+  version='0.0.1',
+  description='oss-dev pypi calculator 124',
+  author='parktaemin'
+  author_email='xoals73771@gmail.com'
+  url="https://github.com/Fromis999/PyPI/"
+  install_requires=['pytest']
+  long_description_content_type='text/markdown',
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+  ],
+)
